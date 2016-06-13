67d0d0029637c9894a6490bc088aa47a9cc9039e
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-06-13 14:52:56 +0200

    now using kill to save time

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 docker rmi regapp_image
 docker build -t regapp_image /var/regapp
 docker network create regapp_network
-docker stop regapp
+docker kill regapp
 docker rm regapp
 docker run -i -t -d --name regapp --net regapp_network regapp_image
