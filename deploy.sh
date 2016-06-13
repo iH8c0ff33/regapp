e1aef4f1131e213e434aa74fe7f1cecbfbc702fc
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-06-13 15:01:24 +0200

    build with polymer

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 docker rmi regapp_image
+cd /var/regapp/app
+polymer build
+cp -a /var/regapp/app/build/unbundled /var/regapp/public
 docker build -t regapp_image /var/regapp
 docker network create regapp_network
 docker kill regapp
