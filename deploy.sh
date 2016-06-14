966974fde1d6cea055ce9e42436c6842649f14dd
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-06-14 23:04:26 +0200

    try with bundled

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cd /var/regapp/app
 bower install
 polymer build
 rm -rf /var/regapp/public
-cp -a /var/regapp/app/build/unbundled /var/regapp/public
+cp -a /var/regapp/app/build/bundled /var/regapp/public
 docker build -t regapp_image /var/regapp
 docker network create regapp_network
 docker kill regapp
